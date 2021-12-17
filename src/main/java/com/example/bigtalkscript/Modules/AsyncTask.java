206e787b488f2393646220fe49c2ebf27c7c035b
jim <813350550@qq.com>
2021-12-17 13:24:07 +0800

    初始版本

--- src/main/java/com/example/bigtalkscript/Modules/AsyncTask.java
+++ src/main/java/com/example/bigtalkscript/Modules/AsyncTask.java
@@ -27,6 +27,7 @@ public class AsyncTask {
     public static void main(String[] args) throws Exception {
         int[] ints = GrabTool.setPoint();
         System.out.println(Arrays.toString(ints));
+        System.out.println(Arrays.toString(ints));
         //int[] ints = new int[]{1368, 2, 204, 204, 204};
         GrabTool.MouseResponse(ints);
     }
